78ffc5a41896c9b435a35fe80bf60d6319eae15c
donnelly-dII <donnellyd215@gmail.com>
2018-10-01 09:58:07 -0700

    File Upload

--- Modeling.hpp
+++ Modeling.hpp
@@ -2,13 +2,41 @@
 //  Modeling.hpp
 //  MoodysPractice
 //
-//  Created by David Donnelly II on 10/1/18.
+//  Created by David Donnelly II on 9/29/18.
 //  Copyright © 2018 DavidDonnellyII. All rights reserved.
 //
 
 #ifndef Modeling_hpp
 #define Modeling_hpp
-
+#include <vector>
 #include <stdio.h>
+#include "DataHandle.hpp"
+using namespace std;
+
+struct Simulation{
+    int iterations;
+    int years;
+    double startMean;
+    double MeanVar;
+    double euro_diff_mean;
+    double euro_diff_var;
+};
+
+class MonteCarlo{
+public:
+    MonteCarlo(Simulation, vector<Firm>);
+    Simulation Simu;
+    double* Run();
+    vector<Firm> FirmsList;
+    double MertonCalc(Firm, int, int);
+private:
+    double* American_GDP_Generator();
+    double* Euro_GDP_Generator(double*);
+    double* SingleIteration();
+    double d2Calc(Firm, int, int);
+    double phi(double);
+};
 
 #endif /* Modeling_hpp */
+
+
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  MoodysPractice
+//
+//  Created by David Donnelly II on 10/1/18.
+//  Copyright © 2018 DavidDonnellyII. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
